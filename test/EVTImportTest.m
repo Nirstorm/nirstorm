07e1550d9f46b117bd796e637f71970430606a71
Edouard DELAIRE <ext-e.delaire@pclx42.meg.chups.jussieu.fr>
2018-06-05 15:37:20 +0200

    adding Utest

--- test/EVTImportTest.m
+++ test/EVTImportTest.m
@@ -19,7 +19,7 @@ classdef EVTImportTest < matlab.unittest.TestCase
     
     methods(TestMethodTeardown)
         function tear_down(testCase)
-            rmdir(testCase.tmp_dir, 's');
+            rmdir(fullfile(nst_get_local_user_dir(),'unittest',testCase.tmp_dir), 's');
             utest_clean_bst();
         end
     end
@@ -28,44 +28,29 @@ classdef EVTImportTest < matlab.unittest.TestCase
         function test_evt_event_import_events(testCase)
             global GlobalData;
             
-            sFiles = {...
-            'Subject01/NIRSCOMP_S01-2017-07-12_001_Hb/data_hb_180423_1149_bl_detrend_band.mat'};
-            RawFiles = {...
-            '/lena13/home_users/users/ext-e.delaire/Desktop/test_event/test_event/NIRSCOMP_S01-2017-07-12_001.evt'};
+            utest_dir=fullfile(nst_get_local_user_dir(),'unittest');
+            if ~exist(fullfile(utest_dir,testCase.tmp_dir), 'dir')
+                mkdir(fullfile(utest_dir,testCase.tmp_dir));
+            end
             
+            events=generate_fake_event(10,2,150);
+            
+            dlmwrite(fullfile(utest_dir,testCase.tmp_dir,'event.evt'),events,'delimiter','\t')
+            %bst_save(fullfile(utest_dir,testCase.tmp_dir,'event.evt'),events);
+            %bst_save(fullfile(utest_dir,'event.evt') ,events);
+
+            
+            nirs = load(fullfile(utest_dir,'dummy.nirs'), '-mat');
+            nirs_dt = diff(nirs.t(1:2));
+            
+            
+            sFiles_dummy = utest_import_nirs_in_bst(fullfile(utest_dir,'dummy.nirs'));
+
             sFiles_processed = bst_process('CallProcess', ...
                 'process_nst_import_evt_events', ...
-                sFiles, [],  ...
-                'evtfile',{RawFiles{1}, ''}, ...
-                'label_cols','ConditionA,ConditionB,ConditionC', ...
-                'confirm_importation', 0);
-                           
-                           
-            assert(~isempty(sFiles_processed));
-            events = get_events(sFiles_processed);
-            
-            assert(length(events) == 3);
-            assert(size(events(1).samples, 1) == 2);
-            
-            %assert(abs(events(strcmp({events.label}, 'DMNirs')).times(1, 1) - 1482333540.553) <= nirs_dt);
-            %assert(abs(events(strcmp({events.label}, 'DMNirs')).times(2, 1) - 1482333570.823) <= nirs_dt);
-            utest_reset_bst();      
-        end
-        
-        
-        function test_evt_event_merge(testCase)
-            global GlobalData;
-            
-            sFiles = {...
-            'Subject01/NIRSCOMP_S01-2017-07-12_001_Hb/data_hb_180423_1149_bl_detrend_band.mat'};
-            RawFiles = {...
-            '/lena13/home_users/users/ext-e.delaire/Desktop/test_event/test_event/NIRSCOMP_S01-2017-07-12_001.evt'};
-            
-            sFiles_processed = bst_process('CallProcess', ...
-                'process_nst_import_evt_events', ...
-                sFiles, [],  ...
-                'evtfile',{RawFiles{1}, ''}, ...
-                'label_cols','ConditionA,ConditionB,ConditionB', ...
+                sFiles_dummy, [],  ...
+                'evtfile',{ fullfile(utest_dir,testCase.tmp_dir,'event.evt')}, ...
+                'label_cols','ConditionA,ConditionB', ...
                 'confirm_importation', 0);
                            
                            
@@ -74,6 +59,61 @@ classdef EVTImportTest < matlab.unittest.TestCase
             
             assert(length(events) == 2);
             assert(size(events(1).samples, 1) == 2);
+            
+            
+            assert( strcmp( events(1).label,'ConditionB'));
+            assert(  size(events(1).epochs,2) == 9 ); 
+            % The last event is not here because we don't know his duration
+            % at it is the last of the file
+
+            assert( strcmp( events(2).label,'ConditionA'));
+            assert(  size(events(2).epochs,2) == 10 );
+
+            
+            assert(abs( events(strcmp({events.label}, 'ConditionA')).times(1, 1)) - 6.04000000000000 <= nirs_dt);
+            assert(abs( events(strcmp({events.label}, 'ConditionA')).times(2, 1)) - 6.04000000000000 <= nirs_dt);
+            
+            utest_reset_bst();      
+        end
+        
+        
+        function test_evt_event_merge(testCase)
+            global GlobalData;
+            
+                        utest_dir=fullfile(nst_get_local_user_dir(),'unittest');
+            if ~exist(fullfile(utest_dir,testCase.tmp_dir), 'dir')
+                mkdir(fullfile(utest_dir,testCase.tmp_dir));
+            end
+            
+            events=generate_fake_event(10,4,150);
+            
+            dlmwrite(fullfile(utest_dir,testCase.tmp_dir,'event.evt'),events,'delimiter','\t')
+            %bst_save(fullfile(utest_dir,testCase.tmp_dir,'event.evt'),events);
+            %bst_save(fullfile(utest_dir,'event.evt') ,events);
+
+            
+            nirs = load(fullfile(utest_dir,'dummy.nirs'), '-mat');
+            nirs_dt = diff(nirs.t(1:2));
+            
+            
+            sFiles_dummy = utest_import_nirs_in_bst(fullfile(utest_dir,'dummy.nirs'));
+
+            sFiles_processed = bst_process('CallProcess', ...
+                'process_nst_import_evt_events', ...
+                sFiles_dummy, [],  ...
+                'evtfile',{ fullfile(utest_dir,testCase.tmp_dir,'event.evt')}, ...
+                'label_cols','ConditionA,ConditionA,ConditionB,ConditionC', ...
+                'confirm_importation', 0);
+            
+            assert(~isempty(sFiles_processed));
+            events = get_events(sFiles_processed);
+            
+            assert(length(events) == 3);
+            assert(events(strcmp({events.label}, 'ConditionA')).times(2, 1) - events(strcmp({events.label}, 'ConditionB')).times(1, 1) - 12 <= nirs_dt);
+            assert(events(strcmp({events.label}, 'ConditionB')).times(2, 1) - events(strcmp({events.label}, 'ConditionB')).times(1, 1) - 6 <= nirs_dt);
+            assert(events(strcmp({events.label}, 'ConditionC')).times(2, 1) - events(strcmp({events.label}, 'ConditionC')).times(1, 1) - 6 <= nirs_dt);
+
+            
             utest_reset_bst();      
         end
         
@@ -84,4 +124,23 @@ end
 function events = get_events(sFile)
 DataMat = in_bst_data(sFile.FileName, 'F');
 events = DataMat.F.events;
+end
+
+
+% Generate a file with n_unique events which repete n times. 
+% Each event last duration sample. 
+
+function fake_event=generate_fake_event(n,unique_event,duration)
+
+fake_event=zeros(n*unique_event,9);
+sample=1;
+
+for i=1:(n*unique_event) 
+        fake_event(i,1)=sample;
+        event_code=dec2bin(1+mod(i,unique_event),8);
+        for j=2:9
+            fake_event(i,j)=str2num(event_code(j-1));          
+        end
+        sample=sample+duration;
+end
 end
