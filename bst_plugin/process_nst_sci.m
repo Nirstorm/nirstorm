f41b55fbbd707bb514a0aaa6800f922f27dbc5e9
tvvincent <20100thomas@gmail.com>
2018-07-17 00:46:37 -0400

    [SCI] validate on simulations. Start validating on real data (WIP)

--- bst_plugin/process_nst_sci.m
+++ bst_plugin/process_nst_sci.m
@@ -4,12 +4,12 @@ function varargout = process_nst_sci( varargin )
 % @=============================================================================
 % This function is part of the Brainstorm software:
 % http://neuroimage.usc.edu/brainstorm
-% 
+%
 % Copyright (c)2000-2017 University of Southern California & McGill University
 % This software is distributed under the terms of the GNU General Public License
 % as published by the Free Software Foundation. Further details on the GPLv3
 % license can be found at http://www.gnu.org/copyleft/gpl.html.
-% 
+%
 % FOR RESEARCH PURPOSES ONLY. THE SOFTWARE IS PROVIDED "AS IS," AND THE
 % UNIVERSITY OF SOUTHERN CALIFORNIA AND ITS COLLABORATORS DO NOT MAKE ANY
 % WARRANTY, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO WARRANTIES OF
@@ -20,37 +20,37 @@ function varargout = process_nst_sci( varargin )
 % =============================================================================@
 %
 % Authors: Thomas Vincent, 2018
-%  
+%
 eval(macro_method);
 end
 
 
 %% ===== GET DESCRIPTION =====
 function sProcess = GetDescription() %#ok<DEFNU>
-    % Description the process
-    sProcess.Comment     = 'Compute SCI';
-    sProcess.Category    = 'Custom';
-    sProcess.SubGroup    = 'NIRS - wip';
-    sProcess.Index       = 1402;
-    sProcess.isSeparator = 0;
-    sProcess.Description = 'https://github.com/Nirstorm/nirstorm/wiki/%5BWIP%5D-Scalp-coupling-index';
-    
-    % Definition of the input accepted by this process
-    sProcess.InputTypes  = {'data','raw'};
-    sProcess.OutputTypes = {'data','raw'};
-    
-    sProcess.nInputs     = 1;
-    sProcess.nMinFiles   = 1;
-    
-    
-    sProcess.options.option_low_cutoff.Comment = 'Bandpass Low cut-off';
-    sProcess.options.option_low_cutoff.Type    = 'value';
-    sProcess.options.option_low_cutoff.Value   = {0.5, 'Hz', 4};
-    
-    sProcess.options.option_high_cutoff.Comment = 'Bandpass High cut-off';
-    sProcess.options.option_high_cutoff.Type    = 'value';
-    sProcess.options.option_high_cutoff.Value   = {2.5, 'Hz', 4}; 
-    
+% Description the process
+sProcess.Comment     = 'Compute SCI';
+sProcess.Category    = 'Custom';
+sProcess.SubGroup    = 'NIRS - wip';
+sProcess.Index       = 1402;
+sProcess.isSeparator = 0;
+sProcess.Description = 'https://github.com/Nirstorm/nirstorm/wiki/%5BWIP%5D-Scalp-coupling-index';
+
+% Definition of the input accepted by this process
+sProcess.InputTypes  = {'data','raw'};
+sProcess.OutputTypes = {'data','raw'};
+
+sProcess.nInputs     = 1;
+sProcess.nMinFiles   = 1;
+
+
+sProcess.options.option_low_cutoff.Comment = 'Bandpass Low cut-off';
+sProcess.options.option_low_cutoff.Type    = 'value';
+sProcess.options.option_low_cutoff.Value   = {0.5, 'Hz', 4};
+
+sProcess.options.option_high_cutoff.Comment = 'Bandpass High cut-off';
+sProcess.options.option_high_cutoff.Type    = 'value';
+sProcess.options.option_high_cutoff.Value   = {2.5, 'Hz', 4};
+
 end
 
 
@@ -60,8 +60,76 @@ function Comment = FormatComment(sProcess)
 Comment = sProcess.Comment;
 end
 
-function OutputFiles = Run(sProcess, sInput)
-    OutputFiles={};
+function OutputFiles = Run(sProcess, sInputs)
+OutputFiles = {};
+for iInput=1:length(sInputs)
+    % Load recordings
+    if strcmp(sInputs(iInput).FileType, 'data')     % Imported data structure
+        sDataIn = in_bst_data(sInputs(iInput).FileName);
+    elseif strcmp(sInputs(iInput).FileType, 'raw')  % Continuous data file
+        sDataIn = in_bst(sInputs(iInput).FileName, [], 1, 1, 'no');
+    end
+    
+
+    ChannelMat = in_bst_channel(sInputs(iInput).ChannelFile);
+    [nirs_ichans, tmp] = channel_find(ChannelMat.Channel, 'NIRS');
+
+    signals = sDataIn.F(nirs_ichans,:);
+    
+    % Bandpass filter
+    low_cutoff = sProcess.options.option_low_cutoff.Value{1};
+    high_cutoff = sProcess.options.option_high_cutoff.Value{1};
+    order = 3;
+    fs = 1 / diff(sDataIn.Time(1:2));
+    [b,a] = butter(order,[low_cutoff*2/fs high_cutoff*2/fs]);
+    signals = filtfilt(b, a, signals')';
+    
+    signals = normalize(signals);
+    
+    % Compute 0-lag cross-correlation
+    pair_indexes = nst_get_pair_indexes_from_names({ChannelMat.Channel(nirs_ichans).Name});
+    sci = zeros(size(signals,1),1);
+    for ipair=1:size(pair_indexes, 1)
+        chan_indexes = pair_indexes(ipair, 1:2); %Consider only 2 first wavelengths
+        sci(chan_indexes) = abs(xcorr(signals(chan_indexes(1), :), ... 
+                                      signals(chan_indexes(2), :), 0, 'coeff'));
+    end
+    
+    % Save time-series data
+    data_out = zeros(size(sDataIn.F, 1), 1);
+    data_out(nirs_ichans,:) = sci;
+    sDataOut = db_template('data');
+    sDataOut.F            = data_out;
+    sDataOut.Comment      = 'SCI';
+    sDataOut.ChannelFlag  = sDataIn.ChannelFlag;
+    sDataOut.Time         = [1];
+    sDataOut.DataType     = 'recordings';
+    sDataOut.nAvg         = 1;
+    sDataOut.DisplayUnits = 'index';
+    
+    % Generate a new file name in the same folder
+    sStudy = bst_get('Study', sInputs(iInput).iStudy);
+    OutputFile = bst_process('GetNewFilename', bst_fileparts(sStudy.FileName), 'data_sci');
+    sDataOut.FileName = file_short(OutputFile);
+    bst_save(OutputFile, sDataOut, 'v7');
+    % Register in database
+    db_add_data(sInputs(iInput).iStudy, OutputFile, sDataOut);
+    
+    [sSubject, iSubject] = bst_get('Subject', sInputs(iInput).SubjectName);
+    panel_protocols('UpdateNode', 'Subject', iSubject);
+    panel_protocols('SelectNode', [], 'subject', iSubject, -1 );
+    % Save database
+    db_save();
+    
+    OutputFiles{iInput} = OutputFile;
+end
 end
 
+function sig = normalize(sig)
+nb_samples = size(sig, 2);
 
+% sig = 2 * (sig - repmat(min(sig,[],2), 1, nb_samples)) ./ ...
+%       repmat(max(sig,[],2)-min(sig,[],2), 1, nb_samples) - 1;
+%sig = sig - repmat(mean(sig,2), 1, nb_samples);
+sig = sig ./ repmat(std(sig, 0, 2), 1, nb_samples);
+end
--- test/SciTest.m
+++ test/SciTest.m
@@ -24,36 +24,93 @@ classdef SciTest < matlab.unittest.TestCase
         
         function test_sci_with_simulations(testCase)
             
-            dt = 0.1; %sec (10Hz)
-            time = (0:100) * dt; %sec
+            dt = 0.05; %sec (20Hz)
+            time = (0:8000) * dt; %sec
             nb_samples = length(time);
             
-            bst_create_test_subject()
+            bst_create_test_subject();
             
             %% Only noise -- uncorrelated wavelength signals
-            signals = [randn(1, nb_samples);randn(1, nb_samples)];
+            signals = [randn(1, nb_samples);randn(1, nb_samples);];
             nirs_input = bst_create_nirs_data('test_all_noisy', signals, time);
-            outputs = bst_process('CallProcess', 'process_nst_sci', [], nirs_input);
-            %Check sci==0
+            output = bst_process('CallProcess', 'process_nst_sci', nirs_input, []);
+            sDataOut = in_bst_data(output.FileName);
             
-            %% Perfect cardiac consistency
+            %Check SCI close to 0
+            testCase.assertTrue(all(sDataOut.F<0.1));
             
-            %Check sci==1
+            %% Correlation outside of cardiac band
+            freq = 5; % Hz
+            signals = [cos(4*pi*freq*time) + randn(1, nb_samples);...
+                       cos(4*pi*freq*time) + randn(1, nb_samples)];
+            nirs_input = bst_create_nirs_data('test_all_noisy', signals, time);
+            output = bst_process('CallProcess', 'process_nst_sci', nirs_input, []);
+            sDataOut = in_bst_data(output.FileName);
             
+            %Check SCI close to 0
+            testCase.assertTrue(all(sDataOut.F<0.1));
             
-            %% Noisy consistent cardiac components - high SNR
+            %% Perfect cardiac consistency - correlated
+            freq = 1; % Hz
+            signals = [cos(4*pi*freq*time); cos(4*pi*freq*time)];
+            nirs_input = bst_create_nirs_data('test_all_noisy', signals, time);
+            output = bst_process('CallProcess', 'process_nst_sci', nirs_input, []);
+            sDataOut = in_bst_data(output.FileName);
             
-            %Check sci > 0.75
+            %Check SCI high
+            testCase.assertTrue(all(sDataOut.F>0.99));
             
-            %% Noisy consistent cardiac components - very low SNR
+            %% Perfect cardiac consistency - anticorrelated
+            freq = 1; % Hz
+            signals = [cos(4*pi*freq*time); -cos(4*pi*freq*time)];
+            nirs_input = bst_create_nirs_data('test_all_noisy', signals, time);
+            output = bst_process('CallProcess', 'process_nst_sci', nirs_input, []);
+            sDataOut = in_bst_data(output.FileName);
             
-            %Check sci < 0.75
+            %Check SCI high
+            testCase.assertTrue(all(sDataOut.F>0.99));
+                        
+            %% Consistent cardiac components - high SNR
+            freq = 1; % Hz
+            signals = [cos(4*pi*freq*time) + randn(1, nb_samples)*0.2; ...
+                       -cos(4*pi*freq*time) + randn(1, nb_samples)*0.2];
+            nirs_input = bst_create_nirs_data('test_all_noisy', signals, time);
+            output = bst_process('CallProcess', 'process_nst_sci', nirs_input, []);
+            sDataOut = in_bst_data(output.FileName);
+            
+            %Check SCI > 0.75
+            testCase.assertTrue(all(sDataOut.F>0.75));
+            
+            %% Consistent cardiac components - very low SNR
+            freq = 1; % Hz
+            signals = [cos(4*pi*freq*time) + randn(1, nb_samples)*50; ...
+                       -cos(4*pi*freq*time) + randn(1, nb_samples)*50];
+            nirs_input = bst_create_nirs_data('test_all_noisy', signals, time);
+            output = bst_process('CallProcess', 'process_nst_sci', nirs_input, []);
+            sDataOut = in_bst_data(output.FileName);
+            
+            %Check SCI < 0.75
+            testCase.assertTrue(all(sDataOut.F<0.75));
             
             
         end
         
         function test_sci_on_tapping_data(testCase)
+            repo_url = nst_get_repository_url();
+            nirs_fns = nst_request_files({{'tutorial_nirs_tapping','data','S01_Block_FO_LH_Run01.nirs'}, ...
+                                          {'tutorial_nirs_tapping','data','optodes.txt'}, ...
+                                          {'tutorial_nirs_tapping','data','fiducials.txt'}}, ...
+                                         1, repo_url);
+            nirs_fn = nirs_fns{1};
+            sFile = utest_import_nirs_in_bst(nirs_fn);
+            output = bst_process('CallProcess', 'process_nst_sci', sFile, []);
+            sDataOut = in_bst_data(output.FileName);
             
+            %TODO: assert non-regression:
+            %  - create frozen results
+            %  - make available online
+            %  - retrieve here
+            %  - compare with computed results
         end
         
         
@@ -62,46 +119,63 @@ end
 
 function OutputFile = bst_create_nirs_data(label, signals, time, chan_names, srcs_pos, dets_pos)
 % TODO: expose, doc, test
-iStudy = db_add_condition('nst_utest', 'test_sci');
+iStudy = db_add_condition('test_subject', label);
 sStudy = bst_get('Study', iStudy);
 
 nb_channels = size(signals,1);
 
 if nargin < 4 || isempty(chan_names)
     chan_names = cell(1, nb_channels);
+    wavelengths = [685 830];
     for ichan=1:nb_channels
-        i_src = round((ichan-1) / 4) + 1;
-        i_det = round((ichan-1) / 2) + 1;
+        i_src = floor((ichan-1) / 4) + 1;
+        i_det = floor((ichan-1) / 2) + 1;
         i_wl = mod(ichan-1,2) + 1;
-        chan_names{ichan} = nst_format_channel(i_src, i_det, i_wl);
+        chan_names{ichan} = nst_format_channel(i_src, i_det, wavelengths(i_wl));
     end
 end
 assert(length(chan_names)==nb_channels);
 
 if nargin < 5 || isempty(srcs_pos)
-    [i_srcs, i_dets, i_wls] = nst_unformat_channels(chan_names); %TODO - also make some checks for consistency
+    [i_srcs, i_dets, measures, ctype] = nst_unformat_channels(chan_names);
     % Default locations: 
     %   one source every 3cm on a straight line at y=0, z=0
     %   one detector every 3cm on a straight line at y=3, z=0
+    srcs_pos = zeros(3, nb_channels);
+    dets_pos = zeros(3, nb_channels);
+    ref_point =  [-0.0789 ; -0.0263 ; 0.0655]; % meter, somewhere at the back of the head when using Colin27_4NIRS
     for ichan=1:nb_channels
-       srcs_pos = [i_srcs(ichan)-1 * 0.03; 0; 0]; %meter
-       dets_pos = [i_dets(ichan)-1 * 0.03; 0.03; 0]; %meter
+       srcs_pos(:, ichan) = ref_point + [(i_srcs(ichan)-1) * 0.03; 0; 0]; %meter
+       dets_pos(:, ichan) = ref_point + [(i_dets(ichan)-1) * 0.03; 0.03; 0]; %meter
     end
 end
 
-if nargin < 6
+if nargin>=5 && nargin < 6
     error('Provide det_pos with src_pos');
 end
 
 % Create channel definition
+[i_srcs, i_dets, measures, ctype] = nst_unformat_channels(chan_names);
+chan_types = nst_channel_types();
 ChannelMat = db_template('channelmat');
+ChannelMat.Comment = sprintf('NIRS-BRS channels (%d)', nb_channels);
 ChannelMat.Channel = repmat(db_template('channeldesc'), 1, nb_channels);
 for ichan=1:nb_channels
-    [isrc, idet] = unformat_channel_name(chan_names{ichan});
     ChannelMat.Channel(ichan).Name = chan_names{ichan};
-    ChannelMat.Channel(ichan).Group = 'NIRS';
-    ChannelMat.Channel(ichan).Loc(:,1) = srcs_pos(:,ichan);
-    ChannelMat.Channel(ichan).Loc(:,2) = dets_pos(:,ichan);
+    ChannelMat.Channel(ichan).Type = 'NIRS';
+    ChannelMat.Channel(ichan).Weight = 1;
+    if ctype == chan_types.WAVELENGTH
+        ChannelMat.Channel(ichan).Group = sprintf('WL%d', measures(ichan));
+    elseif ctype == chan_types.HB
+        ChannelMat.Channel(ichan).Group = measures{ichan};
+    end
+    ChannelMat.Channel(ichan).Loc(:,1) = srcs_pos(:, ichan);
+    ChannelMat.Channel(ichan).Loc(:,2) = dets_pos(:, ichan);
+end
+if ctype == chan_types.WAVELENGTH
+    ChannelMat.Nirs.Wavelengths = unique(measures);
+elseif ctype == chan_types.HB
+    ChannelMat.Nirs.Hb = unique(measures);
 end
 
 % Save channel definition
@@ -111,7 +185,7 @@ db_set_channel(iChannelStudy, ChannelMat, 0, 0);
 % Save time-series data
 sDataOut = db_template('data');
 sDataOut.F            = signals;
-sDataOut.Comment      = label;
+sDataOut.Comment      = 'data';
 sDataOut.ChannelFlag  = ones(nb_channels, 1);
 sDataOut.Time         = time;
 sDataOut.DataType     = 'recordings';
@@ -124,4 +198,5 @@ sDataOut.FileName = file_short(OutputFile);
 bst_save(OutputFile, sDataOut, 'v7');
 % Register in database
 db_add_data(iStudy, OutputFile, sDataOut);
+db_save();
 end
