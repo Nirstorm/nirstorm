43fb67cae362ae1a4ef11d4e519e36d9a8cfae65
Edouard Delaire <edouard.delaire@icloud.com>
2021-10-12 17:16:01 -0400

    Add process to merge montage

---
+++ bst_plugin/OM/process_nst_merge_montage.m
@@ -0,0 +1,168 @@
+function varargout = process_nst_merge_montage( varargin )
+
+% @=============================================================================
+% This function is part of the Brainstorm software:
+% http://neuroimage.usc.edu/brainstorm
+% 
+% Copyright (c)2000-2016 University of Southern California & McGill University
+% This software is distributed under the terms of the GNU General Public License
+% as published by the Free Software Foundation. Further details on the GPLv3
+% license can be found at http://www.gnu.org/copyleft/gpl.html.
+% 
+% FOR RESEARCH PURPOSES ONLY. THE SOFTWARE IS PROVIDED "AS IS," AND THE
+% UNIVERSITY OF SOUTHERN CALIFORNIA AND ITS COLLABORATORS DO NOT MAKE ANY
+% WARRANTY, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO WARRANTIES OF
+% MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE, NOR DO THEY ASSUME ANY
+% LIABILITY OR RESPONSIBILITY FOR THE USE OF THIS SOFTWARE.
+%
+% For more information type "brainstorm license" at command prompt.
+% =============================================================================@
+%
+% Authors: Edouard Delaire, 2020; Thomas Vincent, 2015-2019
+
+
+eval(macro_method);
+end
+
+
+%% ===== GET DESCRIPTION =====
+function sProcess = GetDescription() %#ok<DEFNU>
+    sProcess.Comment     = 'Merge Montage';
+    sProcess.FileTag     = '';
+    sProcess.Category    = 'Custom';
+    sProcess.SubGroup    = {'NIRS', 'Sources'};
+    sProcess.Index       = 1407;
+    sProcess.Description = '';
+    sProcess.isSeparator = 1; 
+    
+    % Definition of the input accepted by this process
+    sProcess.InputTypes  = {'data', 'raw'};
+    % Definition of the outputs of this process
+    sProcess.OutputTypes = {'data', 'raw'};
+    sProcess.nInputs     = 1;
+    sProcess.nMinFiles   = 1;
+    
+%     % Definition of the options
+
+    
+
+end
+
+%% ===== FORMAT COMMENT =====
+function Comment = FormatComment(sProcess) %#ok<DEFNU>
+    Comment = sProcess.Comment;
+end
+
+%% ===== RUN =====
+function OutputFiles = Run(sProcess, sInputs) %#ok<DEFNU>
+
+   OutputFiles = {};
+   
+   n_source = 1;
+   n_det = 1;
+   new_channels = []; 
+   
+   sources_pos = [];
+   det_pos = [];
+   
+   for iFile = 1:length(sInputs)
+        % Load channel file
+        
+        mapSources = containers.Map();
+        mapDet     = containers.Map();
+
+        
+        ChanneMat = in_bst_channel(sInputs(iFile).ChannelFile);
+        channels = ChanneMat.Channel;
+        for i_chan = 1:length(channels)
+            [isrcs, idets, measures, channel_type] = nst_unformat_channel(channels(i_chan).Name);
+            if ~mapSources.isKey(num2str(isrcs))
+                mapSources(num2str(isrcs)) = n_source;
+                sources_pos = [sources_pos channels(i_chan).Loc(:,1) ];
+                n_source = n_source +1;
+            end   
+            if ~mapDet.isKey(num2str(idets))
+                mapDet(num2str(idets)) = n_det;
+                det_pos = [det_pos channels(i_chan).Loc(:,2) ];
+                n_det = n_det +1;
+            end   
+            tmp = sprintf('S%dD%dWL%d', mapSources(num2str(isrcs)), mapDet(num2str(idets)),measures);
+            %disp(sprintf('%s => %s', channels(i_chan).Name, tmp));
+            channels(i_chan).Name = tmp;
+            
+            if isempty(new_channels)
+                new_channels = channels(i_chan);
+            else
+                new_channels = [new_channels ; channels(i_chan)];
+            end    
+        end
+   end
+   
+    disp('source-source conflict')
+    detect_conflict(sources_pos, sources_pos, 1);
+   
+    disp('Det-Det conflict')
+    detect_conflict(det_pos, det_pos , 1 );
+
+    disp('Source-Det conflict')
+    detect_conflict(sources_pos, det_pos, 0);
+   
+    iStudy = db_add_condition(sInputs(1).SubjectName, [sInputs(1).Condition, '_merge2']);
+    sStudy = bst_get('Study', iStudy);
+    
+    
+    ChanneMat.Channel = new_channels;
+    ChanneMat.Comment = sprintf('NIRS-BRS channels (%d)', length(new_channels));
+    % Save channel definition
+    [tmp, iChannelStudy] = bst_get('ChannelForStudy', iStudy);
+    db_set_channel(iChannelStudy, ChanneMat, 0, 0);
+    
+    separations = process_nst_separations('Compute',ChanneMat.Channel) * 100; %convert to cm
+    % Save time-series data
+    sDataOut              = db_template('data');
+    sDataOut.F            = separations;
+    sDataOut.Comment      = 'Separations';
+    sDataOut.ChannelFlag  = ones(length(separations),1);
+    sDataOut.Time         = [1];
+    sDataOut.DataType     = 'recordings';
+    sDataOut.nAvg         = 1;
+    sDataOut.DisplayUnits = 'cm';
+
+    % Generate a new file name in the same folder
+    OutputFile = bst_process('GetNewFilename', bst_fileparts(sStudy.FileName), 'data_chan_dist');
+    sDataOut.FileName = file_short(OutputFile);
+    bst_save(OutputFile, sDataOut, 'v7');
+    % Register in database
+    db_add_data(iStudy, OutputFile, sDataOut);
+    OutputFiles{1} = OutputFile;
+    
+end
+
+function d = euc_dist(p1, p2)
+    d = sqrt(sum((p1 - p2).^2));
+end
+
+function conflict = detect_conflict(pos1, pos2, isSymetrical)
+
+    conflict =cell(1, size(pos1,2));
+    for i = 1:size(pos1,2)
+        D = zeros(1, size(pos2,2));
+        if isSymetrical
+            n2 = i;
+        else
+            n2 = size(pos2,2);
+        end    
+        for j = 1:n2
+            D(j) = euc_dist(pos1(:,i), pos2(:,j));
+        end
+        colapsing = find( D > 0 & D*100 < 1.5);
+        if ~isempty(colapsing)
+              conflict{i} = union( conflict{i}, colapsing)';  
+        end    
+    end  
+    for i=1:length(conflict)
+        if ~isempty(conflict{i})
+            disp(sprintf('%d and %s are conflicting',i,strjoin(strsplit(num2str(conflict{i})), ' - ')))
+       end 
+   end
+end   
