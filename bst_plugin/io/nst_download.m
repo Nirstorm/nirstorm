b25404fa06b7332b3856c2109ab723b4cb6b01ba
Raymundo Cassani <raymundo.cassani@gmail.com>
2024-11-27 14:58:39 -0500

    Update URL for NIRS files to Brainstorm server (#247)
    * Update URL for NIRS files hosted in Brainstorm server
    
    * simplify file download (handled by bst_websave)
    
    ---------
    
    Co-authored-by: Edouard <edouard.delaire@icloud.com>

--- bst_plugin/io/nst_download.m
+++ bst_plugin/io/nst_download.m
@@ -14,52 +14,18 @@ end
 
 success = 1;
 tstart = tic();
-if ~isempty(strfind(source_fn, 'https:')) || ~isempty(strfind(source_fn, 'ftp:')) || ~bst_interactive
-    tmp_download = tempname;
-    fprintf([download_msg ' ....\n']);
-    if ~isempty(strfind(source_fn, 'ftp:'))
-        [ftp_site, source_ftp_rfn] = nst_split_ftp(source_fn);
-        hftp = ftp(ftp_site);
-        tmp_ftp = tempname;
-        mget(hftp, source_ftp_rfn, tmp_ftp);
-        copyfile(fullfile(tmp_ftp, source_ftp_rfn), tmp_download);
-        close(hftp);
-    else 
-        try
-            tmp_download = bst_websave(tmp_download, source_fn);
-        catch ME
-            errMsg = ['Download data failed from:' 10 source_fn];
-            bst_error(errMsg);
-            if exist(tmp_download, 'file')
-                delete(tmp_download);
-            end
-            success = 0;
-            return
-        end
-    end
-    dest_folder = fileparts(dest_fn);
-    if ~exist(dest_folder, 'dir')
-        mkdir(dest_folder);
-    end
-    copyfile(tmp_download, dest_fn);
-elseif ~isempty(strfind(source_fn, 'http'))
-    [root, basename_fn, ext] = fileparts(dest_fn);
-    errMsg = bst_websave(dest_fn, source_fn);
-    % Error message
-    if ~isempty(errMsg)
-        % Try downloading without using bst
-        if ~nst_download(source_fn, dest_fn, download_msg, 0)
-            errMsg = ['Download data failed from:' 10 source_fn];
-            bst_error(errMsg);
-            success = 0;
-            return
-        end
-    end
-else
-    errMsg = ['Unhandled web ressource:' 10 url];
+
+[root, basename_fn, ext] = fileparts(dest_fn);
+errMsg = bst_websave(dest_fn, source_fn);
+
+tend = toc(tstart);
+
+% Error message
+if ~isempty(errMsg)
+    errMsg = ['Download data failed from:' 10 source_fn];
     bst_error(errMsg);
     success = 0;
-    return
 end
-fprintf('%s -- done in %1.1f sec.\n', download_msg, toc(tstart));
+
+fprintf('%s -- done in %1.1f sec.\n', download_msg, tend);
 end
