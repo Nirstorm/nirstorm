2d0914b32f08a3ac2f5781555b2ab46578d3adcc
Edouard <edouard.delaire@icloud.com>
2025-04-30 18:37:48 -0400

    Adapt loading function

--- bst_plugin/forward/nst_headmodel_get_gains.m
+++ bst_plugin/forward/nst_headmodel_get_gains.m
@@ -1,22 +1,43 @@
-function gains = nst_headmodel_get_gains(nirs_head_model,iwl, channel_def, selected_chans)
+function gains = nst_headmodel_get_gains(head_model, unused_iWL, sChannel, selected_channels)
 
-gain_in         = nirs_head_model.Gain;
-gain_pair_names = nirs_head_model.pair_names;
+    if ndims(head_model.Gain) == 3
+        % Old format. Gain is (nChannel/nWavelength) x nWavelength x nVertex
+        all_wavelength      = unique({sChannel.Group});
+        selected_wavelength = unique({sChannel(selected_channels).Group});
+        
+        iWL = find(strcmp(all_wavelength,selected_wavelength));
+        assert(unused_iWL == iWL );
+        
+        gains     = nst_headmodel_get_gains_old(head_model, iWL, sChannel, selected_channels);
+    else
+        % New format. Gain is nChannel  x nDipole (nDipole = nVertex x 3)
+        gains = head_model.Gain(selected_channels, 1:3:end); 
 
-% hash gain pair names
-gpair_idx = containers.Map();
-for gpic=1:length(gain_pair_names)
-    gpair_idx(gain_pair_names{gpic}) = gpic;
-end
-
-gains = zeros(length(selected_chans), size(gain_in, 3));
-for ic=1:length(selected_chans)
-    ichan = selected_chans(ic);
-    chan_name = channel_def(ichan).Name;
-    pair_name = chan_name(1:strfind(chan_name, 'WL')-1);
-    if ~isempty(pair_name) && gpair_idx.isKey(pair_name)
-        gains(ic, :) = squeeze(gain_in(gpair_idx(pair_name), iwl, :));
     end
+
 end
+
+
+
+function gains = nst_headmodel_get_gains_old(nirs_head_model,iwl, channel_def, selected_chans)
+
+    gain_in         = nirs_head_model.Gain;
+    gain_pair_names = nirs_head_model.pair_names;
+    
+    % hash gain pair names
+    gpair_idx = containers.Map();
+    for gpic=1:length(gain_pair_names)
+        gpair_idx(gain_pair_names{gpic}) = gpic;
+    end
+    
+    gains = zeros(length(selected_chans), size(gain_in, 3));
+    for ic=1:length(selected_chans)
+        ichan = selected_chans(ic);
+        chan_name = channel_def(ichan).Name;
+        pair_name = chan_name(1:strfind(chan_name, 'WL')-1);
+        if ~isempty(pair_name) && gpair_idx.isKey(pair_name)
+            gains(ic, :) = squeeze(gain_in(gpair_idx(pair_name), iwl, :));
+        end
+    end
 
 end
--- bst_plugin/forward/process_nst_extract_sensitivity_from_head_model.m
+++ bst_plugin/forward/process_nst_extract_sensitivity_from_head_model.m
@@ -109,7 +109,7 @@ ChannelFlag = bst_chan_data.ChannelFlag;
 
 head_model = in_bst_headmodel(sStudy.HeadModel(sStudy.iHeadModel).FileName);
 ChannelMat = in_bst_channel(sInputs(1).ChannelFile);
-cortex = in_tess_bst(head_model.SurfaceFile);
+sCortex    = in_tess_bst(head_model.SurfaceFile);
 
 
 
@@ -118,11 +118,6 @@ if ~strcmp(head_model.HeadModelType, 'surface')
     return;
 end
 
-
-if ndims(head_model.Gain) ~= 3
-   bst_error('Bad shape of gain matrix, must be nb_pairs x nb_wavelengths x nb_vertices');
-   return;
-end
  
 montage_info = nst_montage_info_from_bst_channels(ChannelMat.Channel,ChannelFlag);
 
@@ -132,8 +127,8 @@ det_coords = montage_info.det_pos;
 nb_sources          = size(src_coords, 1);
 nb_dets             = size(det_coords, 1);
 
-nb_nodes            = size(head_model.Gain   , 3);
-nb_Wavelengths      = size(head_model.Gain   , 2);
+nb_nodes            = size(sCortex.Vertices   , 1);
+nb_Wavelengths      = length(ChannelMat.Nirs.Wavelengths);
 
 time        = 1:(nb_sources*100 + nb_dets + 1);
 isUsedTime  = zeros(1, length(time));
@@ -142,12 +137,13 @@ sensitivity_surf        = zeros(nb_nodes, nb_Wavelengths, length(time));
 sensitivity_surf_sum    = zeros(nb_nodes, nb_Wavelengths);
 
 %% Compute sensitivity
-for iwl=1:nb_Wavelengths
+for iwl = 1:nb_Wavelengths
+
     swl = ['WL' num2str(ChannelMat.Nirs.Wavelengths(iwl))];
     selected_chans = strcmpi({ChannelMat.Channel.Group}, swl) & (ChannelFlag>0)';
     idx_chan       = find(selected_chans);
         
-    sensitivity     = nst_headmodel_get_gains(head_model,iwl, ChannelMat.Channel,idx_chan );
+    sensitivity         = nst_headmodel_get_gains(head_model, iwl, ChannelMat.Channel, idx_chan);
 
 
     for iChan = 1:length(idx_chan)
@@ -207,10 +203,10 @@ for iwl=1:size(sensitivity_surf, 2)
     OutputFiles{end+1} = ResultFile;
 
 
-        [sStudy, ResultFile] = add_surf_data( squeeze(sensitivity_surf(:,iwl,:)), time, ...
-            head_model, ['Sensitivities - WL' num2str(iwl)], ...
-            sInputs.iStudy, sStudy, 'sensitivity imported from MCXlab');
-        OutputFiles{end+1} = ResultFile;
+    [sStudy, ResultFile] = add_surf_data( squeeze(sensitivity_surf(:,iwl,:)), time, ...
+        head_model, ['Sensitivities - WL' num2str(iwl)], ...
+        sInputs.iStudy, sStudy, 'sensitivity imported from MCXlab');
+    OutputFiles{end+1} = ResultFile;
 end
 
 
@@ -262,23 +258,23 @@ end
 
 %% define the reconstruction FOV
 thresh_dis2cortex           = sProcess.options.thresh_dis2cortex.Value{1}*0.01;
-[valid_nodes,dis2cortex]    = nst_headmodel_get_FOV(ChannelMat, cortex, thresh_dis2cortex, ChannelFlag);
+[valid_nodes,dis2cortex]    = nst_headmodel_get_FOV(ChannelMat, sCortex, thresh_dis2cortex, ChannelFlag);
 
-if any(strcmp({cortex.Atlas.Name},'NIRS-FOV'))
-    iAtlas = find(strcmp({cortex.Atlas.Name},'NIRS-FOV'));
+if any(strcmp({sCortex.Atlas.Name},'NIRS-FOV'))
+    iAtlas = find(strcmp({sCortex.Atlas.Name},'NIRS-FOV'));
 else
-    cortex.Atlas(end+1).Name = 'NIRS-FOV';
-    iAtlas = length( cortex.Atlas);
+    sCortex.Atlas(end+1).Name = 'NIRS-FOV';
+    iAtlas = length( sCortex.Atlas);
 end
 
 
-cortex.Atlas(iAtlas).Scouts(end+1)              = db_template('Scout'); 
-cortex.Atlas(iAtlas).Scouts(end).Vertices       = valid_nodes;
-cortex.Atlas(iAtlas).Scouts(end).Seed           = valid_nodes(1);
-cortex.Atlas(iAtlas).Scouts(end).Label          = sprintf('NIRS FOV (%d cm)',sProcess.options.thresh_dis2cortex.Value{1} );
-cortex.Atlas(iAtlas).Scouts(end)                = panel_scout('SetColorAuto',cortex.Atlas(iAtlas).Scouts(end), length(cortex.Atlas(iAtlas).Scouts));
+sCortex.Atlas(iAtlas).Scouts(end+1)              = db_template('Scout'); 
+sCortex.Atlas(iAtlas).Scouts(end).Vertices       = valid_nodes;
+sCortex.Atlas(iAtlas).Scouts(end).Seed           = valid_nodes(1);
+sCortex.Atlas(iAtlas).Scouts(end).Label          = sprintf('NIRS FOV (%d cm)',sProcess.options.thresh_dis2cortex.Value{1} );
+sCortex.Atlas(iAtlas).Scouts(end)                = panel_scout('SetColorAuto',sCortex.Atlas(iAtlas).Scouts(end), length(sCortex.Atlas(iAtlas).Scouts));
 
-bst_save(file_fullpath(head_model.SurfaceFile), cortex)
+bst_save(file_fullpath(head_model.SurfaceFile), sCortex)
 
 % [sStudy, ResultFile] = add_surf_data(repmat(dis2cortex*100, [1,2]), [0 1], ...
 %                                  head_model, 'Distance to cortex', ...
