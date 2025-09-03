Nirstorm/nirstorm

commit 00555a735155129dd59b1dd1044e480a1d9c5b4a

[bugfix] headmodel -- better detected old head model (#283)
* better check for old head model

* update version

* only check gain dimension

bst_plugin/forward/process_nst_extract_sensitivity_from_head_model.m
@@ -116,7 +116,8 @@ function OutputFiles = Run(sProcess, sInputs)
     end
     
     head_model = in_bst_headmodel(sStudy.HeadModel(sStudy.iHeadModel).FileName, 1);
-    if ~isfield(head_model, 'NIRSMethod') && ndims(head_model.Gain) == 3
+    if ndims(nirs_head_model.Gain) == 3
+        ChannelMat = in_bst_channel(sInputs(1).ChannelFile);
         head_model = process_nst_import_head_model('convert_head_model', ChannelMat, head_model, 0);
     end
     

bst_plugin/VERSION
@@ -1,1 +1,1 @@
-stable_v0.9.4
+stable_v0.9.5

bst_plugin/inverse/process_nst_cmem.m
@@ -151,7 +151,7 @@ function sResults = Compute(OPTIONS, ChannelMat, sDataIn )
 
 
     nirs_head_model = in_bst_headmodel(OPTIONS.HeadModelFile, 1);
-    if ~isfield(nirs_head_model, 'NIRSMethod') && ndims(nirs_head_model.Gain) == 3
+    if ndims(nirs_head_model.Gain) == 3
         nirs_head_model = process_nst_import_head_model('convert_head_model', ChannelMat, nirs_head_model, 0);
     end
 

bst_plugin/inverse/process_nst_wmne.m
@@ -171,7 +171,7 @@ end
 function sResults = Compute(OPTIONS, ChannelMat, sDataIn )
 
     nirs_head_model = in_bst_headmodel(OPTIONS.HeadModelFile, 1);
-    if ~isfield(nirs_head_model, 'NIRSMethod') && ndims(nirs_head_model.Gain) == 3
+    if ndims(nirs_head_model.Gain) == 3
         nirs_head_model = process_nst_import_head_model('convert_head_model', ChannelMat, nirs_head_model, 0);
     end
 

commit 1133804fe520aac906c24d12c4d55afa1ce8baba

Update process_nst_extract_sensitivity_from_head_model.m
Fix typo introduced in https://github.com/Nirstorm/nirstorm/pull/283

bst_plugin/forward/process_nst_extract_sensitivity_from_head_model.m
@@ -116,7 +116,7 @@ function OutputFiles = Run(sProcess, sInputs)
     end
     
     head_model = in_bst_headmodel(sStudy.HeadModel(sStudy.iHeadModel).FileName, 1);
-    if ndims(nirs_head_model.Gain) == 3
+    if ndims(head_model.Gain) == 3
         ChannelMat = in_bst_channel(sInputs(1).ChannelFile);
         head_model = process_nst_import_head_model('convert_head_model', ChannelMat, head_model, 0);
     end