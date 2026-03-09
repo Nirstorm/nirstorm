76c86249c7d75d37d3cc4683bcc4bb74e5137ed7
Edouard <edouard.delaire@icloud.com>
2026-03-09 12:06:47 -0400

    fix nst_get_formats for montage with > 100 optodes

--- bst_plugin/io/nst_get_formats.m
+++ bst_plugin/io/nst_get_formats.m
@@ -1,5 +1,5 @@
 function formats = nst_get_formats()
-formats.pair_re = 'S(?<src_id>[0-9]{1,2})D(?<det_id>[0-9]{1,2})';
+formats.pair_re = 'S(?<src_id>\d+)D(?<det_id>\d+)';
 formats.pair_fmt = 'S%dD%d';
 formats.chan_re = [formats.pair_re '(?<measure>WL\d+|HbO|HbR|HbT)'];
 formats.pair_fmt = 'S%dD%d';
